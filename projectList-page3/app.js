72ac47f4b38257def878210b3a44248e97af8033
ritheesh4 <ritheesh.4@gmail.com>
2020-07-24 17:11:26 +0530

    code for second page scrolling temporarily implemented

--- projectList-page3/app.js
+++ projectList-page3/app.js
@@ -1,4 +1,52 @@
 const mainBody = document.querySelector("main");
+const arrowRight = document.getElementById("navRight");
+const arrowLeft = document.getElementById("navLeft");
+const carouselProgress = document.getElementById("carouselProgress");
+const pageNumber = document.getElementById('pageNumber');
+
+let rightClick = 0;
+let leftClick = 0;
+let divisionLength = 3;
+arrowLeft.style = 'display:none';
+let plotDataLength = 0;
+
+
+// Checking the status of the right arrow button. If the scroll reached to end, button hides.
+const rightArrowStatus = () => {
+    if (rightClick === 2){
+        leftClick ==0;
+        arrowRight.style = 'display:none'
+    } else {
+        rightClick++;
+        console.log(plotDataLength)
+        pageNumber.innerHTML =`${3*(rightClick+1)}/${plotDataLength}`
+        arrowLeft.style = 'display:block'
+        if(leftClick !== 0){
+            leftClick--;
+        }
+    }
+
+  if (rightClick < divisionLength) {
+    carouselProgress.style = `width:33.3%; margin-left:${33.33 * rightClick}%;transition: all 700ms;`;
+  }
+};
+
+const leftArrowStatus = () => {
+    if (leftClick === 2) {
+        rightClick == 0;
+        arrowLeft.style = 'display:none;'
+      } else {
+        leftClick++;
+        pageNumber.innerHTML =`${plotDataLength -(3*(leftClick))}/${plotDataLength}`
+        rightClick--;
+        arrowRight.style = 'display:block'
+      }
+  if (leftClick < divisionLength) {
+    carouselProgress.style = `width: 33.33%; float:right; margin-right:${
+      33.33 * leftClick
+    }%;transition: all 700ms;`;
+  } 
+};
 
 // This function loads the data for the plot cards when the page first loaded.
 const loadPlotData = (e) => {
@@ -8,8 +56,10 @@ const loadPlotData = (e) => {
     xhr.onload = function () {
       if (this.status === 200) {
         const data = JSON.parse(this.responseText);
+        plotDataLength = data.length;
+        pageNumber.innerHTML =`${3}/${plotDataLength}`
+        console.log(plotDataLength)
         data.forEach((element) => {
-            console.log(mainBody.classList)
           mainBody.innerHTML += `<div class="card" id="${element.id}">
           <div class="img-container">
             <img
@@ -63,3 +113,12 @@ const loadPlotData = (e) => {
 
 loadPlotData();
 
+arrowRight.addEventListener("click", function () {
+  mainBody.scrollBy(1200, 0);
+  rightArrowStatus();
+});
+
+arrowLeft.addEventListener("click", function () {
+  mainBody.scrollBy(-1200, 0);
+  leftArrowStatus();
+});
